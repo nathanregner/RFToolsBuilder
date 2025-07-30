9985b909d2835de510b01ee2015eccb01ae28a94
Nathan Regner <nathanregner@gmail.com>
2025-07-29 23:14:08 -0600

    Add scan_complete event

--- src/main/java/mcjty/rftoolsbuilder/modules/cc_tweaked/CCTweakedModule.java
+++ src/main/java/mcjty/rftoolsbuilder/modules/cc_tweaked/CCTweakedModule.java
@@ -1,16 +1,18 @@
 package mcjty.rftoolsbuilder.modules.cc_tweaked;
 
-import dan200.computercraft.api.ComputerCraftAPI;
+import dan200.computercraft.api.peripheral.PeripheralCapability;
 import mcjty.lib.modules.IModule;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+
+import static mcjty.rftoolsbuilder.modules.builder.BuilderModule.BUILDER;
 
 public class CCTweakedModule implements IModule {
 
     @Override
     public void init(FMLCommonSetupEvent event) {
-        registerPeripherals();
     }
 
     @Override
@@ -18,11 +20,10 @@ public class CCTweakedModule implements IModule {
     }
 
     @Override
-    public void initConfig(IEventBus iEventBus) {
-    }
-
-    private void registerPeripherals() {
-        ComputerCraftAPI.registerGenericSource(new BuilderPeripheral());
+    public void initConfig(IEventBus bus) {
+        bus.addListener((RegisterCapabilitiesEvent event) -> {
+            event.registerBlockEntity(PeripheralCapability.get(), BUILDER.be().get(), (b, d) -> new BuilderPeripheral(b));
+        });
     }
 
 }
